cc1dc3b7b59d094cdfb65ac206b53f209833dd97
Seth Battis <seth@battis.net>
2022-04-20 10:50:23 -0400

    Initial Commit

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import ModalEdit, { ForeignKeyLookupFunction, ForeignKeyType, ModalEditConfig, ModalEditTarget } from "./src/ModalEdit";
+export { ModalEdit, ModalEditConfig, ModalEditTarget, ForeignKeyType, ForeignKeyLookupFunction, };
---
+++ index.ts
@@ -0,0 +1,14 @@
+import ModalEdit, {
+  ForeignKeyLookupFunction,
+  ForeignKeyType,
+  ModalEditConfig,
+  ModalEditTarget,
+} from "./src/ModalEdit";
+
+export {
+  ModalEdit,
+  ModalEditConfig,
+  ModalEditTarget,
+  ForeignKeyType,
+  ForeignKeyLookupFunction,
+};
